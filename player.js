948b7c74508b4aa03418f9e9e4bbe3378aec9316
ryaniscool-cyber <ryvn.antony@gmail.com>
2026-01-20 17:09:58 -0700

    Implement player movement controls using THREE.js

--- player.js
+++ player.js
@@ -1,1 +1,25 @@
+import * as THREE from "three";
+
+const velocity = new THREE.Vector3();
+const direction = new THREE.Vector3();
+const keys = {};
+
+document.addEventListener("keydown", e => keys[e.code] = true);
+document.addEventListener("keyup", e => keys[e.code] = false);
+
+export function updatePlayer(controls, delta) {
+  velocity.x -= velocity.x * 10 * delta;
+  velocity.z -= velocity.z * 10 * delta;
+
+  direction.z = Number(keys.KeyW) - Number(keys.KeyS);
+  direction.x = Number(keys.KeyD) - Number(keys.KeyA);
+  direction.normalize();
+
+  const speed = 10;
+  velocity.z -= direction.z * speed * delta;
+  velocity.x -= direction.x * speed * delta;
+
+  controls.moveRight(-velocity.x * delta);
+  controls.moveForward(-velocity.z * delta);
+}
 
